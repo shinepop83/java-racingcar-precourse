5d4401f6f32343793ecf7aeccd96f25246873512
shin <shin@LAPTOP-MM4U4FF8>
2021-05-03 03:36:02 +0900

    - feat : 자동차 경주 게임 mainController 테스트

---
+++ src/test/java/racingcar/controller/RacingCarMainControllerTest.java
@@ -0,0 +1,12 @@
+package racingcar.controller;
+
+import org.junit.jupiter.api.Test;
+
+class RacingCarMainControllerTest {
+
+	@Test
+	void testPlay() {
+		RacingCarMainController.play("SHIN,SUNG,BUN,GAME",5);
+	}
+
+}
